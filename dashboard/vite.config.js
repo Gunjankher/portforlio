5b3d54d1d0bdb0f3a90e0f6380dae0c4a31b2cc3
Gunjankher <gkhercode@gmail.com>
2025-05-02 22:27:54 +0530

    changes

--- dashboard/vite.config.js
+++ dashboard/vite.config.js
@@ -7,7 +7,7 @@ export default defineConfig({
   plugins: [react()],
   server:{
     proxy:{
-      '/api':'https://portforlio-brys.onrender.com/api/v1'
+      '/api':'https://localhost:5000/api/v1'
     }
   },
   resolve: {
